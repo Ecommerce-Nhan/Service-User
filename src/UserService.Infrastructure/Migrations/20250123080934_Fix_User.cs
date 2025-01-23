3f926570ff69037e2ae928829b3c5ace2cd98a17
Tran Thanh Nhan <nhancmvn12@gmail.com>
2025-01-23 15:10:14 +0700

    Refactor: fix type column user

---
+++ src/UserService.Infrastructure/Migrations/20250123080934_Fix_User.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DeleteBy",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreateBy",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "DeleteBy",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "CreateBy",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
