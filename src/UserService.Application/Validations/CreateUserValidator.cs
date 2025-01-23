af567664dae315f1fa9403ac956e176b8e172d0f
Tran Thanh Nhan <nhancmvn12@gmail.com>
2025-01-23 15:06:50 +0700

    Feat: user create validation

---
+++ src/UserService.Application/Validations/CreateUserValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using SharedLibrary.Dtos.Users;
+using UserService.Entities;
+
+namespace UserService.Application.Validations;
+
+public class CreateUserValidator : AbstractValidator<CreateUserDto>
+{
+    private readonly UserManager<User> userManager;
+    public CreateUserValidator(UserManager<User> userManager)
+    {
+        this.userManager = userManager;
+        RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required.")
+                             .EmailAddress().WithMessage("Invalid email format.")
+                             .MustAsync(BeUniqueEmail).WithMessage("Email is already taken.");
+    }
+    private async Task<bool> BeUniqueEmail(string email, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(email)) return false;
+        var user = await userManager.FindByEmailAsync(email);
+        return user == null;
+    }
+}
