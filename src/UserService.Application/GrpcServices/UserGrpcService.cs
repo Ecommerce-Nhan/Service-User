6460844f7927a492dc681709d280e7303f984918
Nhân Trần Thanh <nhancmvn12@gmail.com>
2025-01-27 06:29:16 +0700

    Refactor: gRPC service

--- src/UserService.Application/GrpcServices/UserGrpcService.cs
+++ src/UserService.Application/GrpcServices/UserGrpcService.cs
@@ -15,19 +15,24 @@ public class UserGrpcService(SignInManager<User> signInManager, UserManager<User
         {
             throw new RpcException(new Status(StatusCode.NotFound, "User not found."));
         }
+        var passwordValid = await userManager.CheckPasswordAsync(user, request.Password);
+        if (!passwordValid)
+        {
+            throw new RpcException(new Status(StatusCode.Unauthenticated, "Invalid username or password."));
+        }
         var result = await signInManager.PasswordSignInAsync(request.Username, request.Password, false, false);
         if (!result.Succeeded)
         {
             throw new RpcException(new Status(StatusCode.Unauthenticated, "Invalid username or password."));
         }
         var roleUser = await userManager.GetRolesAsync(user);
-        if (!roleUser.Any())
-        {
-            throw new RpcException(new Status(StatusCode.PermissionDenied, "User does not have any assigned roles."));
-        }
+        //if (!roleUser.Any())
+        //{
+        //    throw new RpcException(new Status(StatusCode.PermissionDenied, "User does not have any assigned roles."));
+        //}
         var response = new LoginModel();
         response.UserId = user.Id;
-        response.UserRole = roleUser.FirstOrDefault();
+        response.UserRole = "roleUser.FirstOrDefault()";
 
         return response;
     }
